834e7e0de085bc4f149de94aa2e3c829d6f018a5
nainaistar <nainaistar@gmail.com>
2020-05-07 23:24:34 +0900

    [Add] CIでデプロイする用のロジック修正

--- src/initializers/firebase.ts
+++ src/initializers/firebase.ts
@@ -1,20 +1,15 @@
 import * as admin from "firebase-admin";
 
-// productionはCI/CDでしばらくは行わないのdえ、必要になったら復活させる。
-// if (process.env.NODE_ENV === `production`) {
-//   admin.initializeApp({
-//     credential: admin.credential.applicationDefault(),
-//   })
-// } else {
-//   const serviceAccount = require(`../../serviceAccountKey.json`)
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   })
-// }
-const serviceAccount = require("../../serviceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (process.env.NODE_ENV === `production`) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  })
+} else {
+  const serviceAccount = require(`../../serviceAccountKey.json`)
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+}
 
 export const firestore = admin.firestore();
 export const FieldValue = require("firebase-admin").firestore.FieldValue;
