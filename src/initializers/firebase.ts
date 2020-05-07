908f799911e2f0175e7b3402c75ee5ff5052e687
nainaistar <nainaistar@gmail.com>
2020-05-07 23:39:47 +0900

    [debug] パワーデバッグ

--- src/initializers/firebase.ts
+++ src/initializers/firebase.ts
@@ -1,10 +1,16 @@
 import * as admin from "firebase-admin";
 
+console.log("ProcessEnv");
+console.log(process.env.SLACK_BOT_TOKEN);
+console.log(process.env.SLACK_SIGNING_SECRET);
+
 if (process.env.NODE_ENV === `production`) {
+  console.log("あいうえお");
   admin.initializeApp({
     credential: admin.credential.applicationDefault(),
   })
 } else {
+  console.log("かきくけこ");
   const serviceAccount = require(`../../serviceAccountKey.json`)
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
